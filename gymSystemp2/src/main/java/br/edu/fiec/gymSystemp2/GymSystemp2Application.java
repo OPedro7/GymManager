08d2fbf4b3fe3978c04c08917b6ebb10b977a8be
PEDRO HENRIQUE ESTRIOLI <46132@fiecalunos.lan>
2026-06-01 19:53:12 -0300

    adicionando arquivos SpringBoot

---
+++ gymSystemp2/src/main/java/br/edu/fiec/gymSystemp2/GymSystemp2Application.java
@@ -0,0 +1,13 @@
+package br.edu.fiec.gymSystemp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymSystemp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymSystemp2Application.class, args);
+	}
+
+}
